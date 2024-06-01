52d34c6b611cb4b27b3f2a4f95cff5994b1aade1
Nooorrrr <noor.bnhdd@gmail.com>
2024-06-01 21:23:55 +0200

    added pop up when invalid login

--- index.php
+++ index.php
@@ -18,8 +18,9 @@
             <div class="form-input">
                 <input type="password" id="password" name="password" placeholder="mot de passe" required>
             </div>
-
-        <!----hna u should add a condition or something te3 l authentification so that it wouldnt redirect u to the page based on the userli dar authentification (i think its doable b js or something) since am working just on the first 2 pages for now 7aitit the lin te3 hadik page f href or u can do it b php-->
+           
+        <!----
+        hna u should add a condition or something te3 l authentification so that it wouldnt redirect u to the page based on the userli dar authentification (i think its doable b js or something) since am working just on the first 2 pages for now 7aitit the lin te3 hadik page f href or u can do it b php-->
             <button class="button" type="submit" >Login</button>
         </form>
     </div>
